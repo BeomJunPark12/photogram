305540dbba296db244fea85a92b53ae89b241c13
beomjunpark <pbeomjun12@gmail.com>
2023-07-10 15:41:15 +0900

    이미지

---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,47 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    @Transactional
+    public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+        UUID uuid = UUID.randomUUID();  // uuid
+        String imageFileName = uuid + "_" +  imageUploadDto.getFile().getOriginalFilename();  // 1.jpg
+        System.out.println("이미지 파일이름: " + imageFileName);
+
+        Path imageFilepath = Paths.get(uploadFolder + imageFileName);
+
+        // 통신, I/O -> 예외가 발생할 수 있다.
+        try {
+            Files.write(imageFilepath, imageUploadDto.getFile().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // DB에 저장
+        Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
+        Image imageEntity = imageRepository.save(image);
+        System.out.println("imageEntity = " + imageEntity);
+    }
+}
