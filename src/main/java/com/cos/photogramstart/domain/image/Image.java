85bd776200dca95c9e2d8dc2f6430047f8e58073
beomjunpark <pbeomjun12@gmail.com>
2023-07-15 11:26:44 +0900

    이미지 스토리

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,8 +25,9 @@ public class Image {
 
     private String postImageUrl;    //  사진을 전송받아서 그 사진을 서버에 특정 폴더에 저장 -DB에 그 저장된 경로를 INSERT
 
+    @JsonIgnoreProperties({"images"})
     @JoinColumn(name = "userId")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER) // 이미지 select하면 조인해서 User정보를 같이 들고옴
     private User user;
 
     private LocalDateTime createDate;
